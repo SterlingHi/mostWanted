1ad4e608290119193a4b30dd2a616e5d37079493
Andy Wade <andyandamandawade@hotmail.com>
2021-06-07 11:46:02 -0500

    instructor feedback

--- mostWanted.js
+++ mostWanted.js
@@ -1,29 +1,86 @@
 'use strict';
 
-function searchByName(){
+function search(){
     // Grabbing the values from our nameForm form and inputs.
     let firstNameInput = document.forms['nameForm']['fname'].value;
     let lastNameInput = document.forms['nameForm']['lname'].value;
+    let eyeColorInput = document.forms['nameForm']['eyecolor'].value;
+    let genderInput = document.forms['nameForm']['gender'].value;
 
+
+    let results = people;
+    if(eyeColorInput != ""){
+        results = searchByEyeColor(eyeColorInput, results)
+    }
+    if(genderInput != ""){
+        results = searchByGender(genderInput, results)
+    }
+
+
+ 
+   
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
-    let filteredPeople = people.filter(function (person) {
-        if(person.firstName === firstNameInput && person.lastName === lastNameInput){
-            return true;
-        }
-        return false;
-    });
+   
     
     // Rather than console logging, you need to append the filteredPeople to a table.
-    if(filteredPeople.length > 0){
-        console.log(filteredPeople);
+    if(results.length > 0){
+        console.log(results);
+        buildTable(results)
     }else{
         console.log('Sorry, looks like there is no one with that name.');
     }    
 }
 
-function showPeople(){
-
-    console.log(people);
-
-
+function searchByName(firstName, lastName, peopleToSearch){
+    let filteredPeople = peopleToSearch.filter(function (person) {
+        if(person.firstName === firstNameInput && person.lastName === lastNameInput){
+            return true;
+        }
+        return false;
+    });
+    return filteredPeople;
 }
+
+function searchByEyeColor(eyeColor, peopleToSearch){
+    let filteredPeople = peopleToSearch.filter(function(el){
+        if(el.eyeColor == eyeColor){
+            return true;
+        }
+        else{
+            return false;
+        }
+    })
+    console.log(filteredPeople)
+    return filteredPeople;
+}
+
+function searchByGender(gender, peopleToSearch){
+    let filteredPeople = peopleToSearch.filter(function(el){
+        if(el.gender == gender){
+            return true;
+        }
+        else{
+            return false;
+        }
+    })
+    console.log(filteredPeople)
+    return filteredPeople;
+}
+
+function doStuff(){
+	searchByEyeColor('blue', people)
+}
+function buildTable(peopleToDisplay){
+    document.getElementById("persons").innerHTML = ""
+	peopleToDisplay.map(function(el){
+		document.getElementById("persons").innerHTML += `<tr>
+		<td>${el.firstName}</td>
+		<td>${el.lastName}</td>
+        <td>${el.dob}</td>
+		<td><button onclick="deletePerson()">Delete</button></td>
+		</tr>`
+	})
+}
+buildTable(people);
+
+//
